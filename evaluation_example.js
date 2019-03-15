6a2a44ff27f4f6f8612126a1c216310fade94424
Taylor Downs <taylor@openfn.org>
2019-03-15 11:54:09 +0000

    Update evaluation_example.js

--- evaluation_example.js
+++ evaluation_example.js
@@ -1,16 +1,22 @@
+// please upload sample submission JSON from commcare.
+
 steps(
-  upsert('Amp__Submission__c', 'Submission_Ext_Id__c', fields(
-    field('Submission_Ext_Id__c', dataValue('form.submissionId'),
-    relationship('Amp__Project__r', dataValue('form.communityId'),
-    field('Date__c', dataValue('form.date'))
+  upsert('ampi__Submission__c', 'CommCare_Id__c', fields(
+    field('CommCare_Id__c', dataValue('form.meta.submissionId')),
+    relationship('ampi__Parent_Submission__c', dataValue('???')),
+    relationship('Amp__Project__r', dataValue('form.communityId'))
   )),
   each(
     state.data.form.questions[*],
-    upsert('Amp__Question_Response__c', 'Response_Id__c', fields(
-      field('Response_Id__c', dataValue('submissionId'),
-      relationship('Amp__Submission__r', dataValue('submissionId'),
-      relationship('Question_r', dataValue('questionId')),
-      field('Value__c', dataValue('response'))
+    upsert('ampi__Question__c', 'Response_Id__c', fields(
+      field('Response_Id__c', state.data.form.meta.submissionId + question_number),
+      relationship('ampi__Parent_Question__r', dataValue('questionId'),
+      field('ampi__Description__c', state.question.description),
+      field('ampi__Response_Type__c', state.question.type),
+      // function returns either
+        field('ampi__Text_Response__c', ...),
+        field('ampi__Number_Response__c', ...),
+        field('ampi__Picklist_Response__c', ...),
     ))
   )
 );
