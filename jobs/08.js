7e96447b5998293c263ec74c98fe2771b7ea0aba
Aleksa Krolls <aleksagk@gmail.com>
2019-12-05 08:45:49 +0200

    Update GPS Coordonnees_GPS for lat

--- jobs/08.js
+++ jobs/08.js
@@ -8,7 +8,7 @@ upsert(
       'Location__latitude__s',
       state => state.data.form.Coordonnees_GPS.split(' ')[0]
     ),
-    field('Location__longitude__s', state => state.data.form.GPS.split(' ')[1]),
+    field('Location__longitude__s', state => state.data.form.Coordonnees_GPS.split(' ')[1]),
     field('Submission_ID__c', dataValue('id')),
     relationship(
       'Project__r',
